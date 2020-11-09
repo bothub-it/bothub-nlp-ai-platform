a25979014db08c7c4509adc1cb74e72eb1ca2408
Lucas Agra <lucasomena@hotmail.com>
2020-11-09 17:23:17 -0300

    debugging download model path

--- download_models.py
+++ download_models.py
@@ -126,11 +126,12 @@ def download_file(url, file_name):
 
 def download_bert(model_name):
     os.makedirs(model_name, exist_ok=True)
+    print("Downloading model to: ", os.path.join(os.getcwd(), model_name))
 
     from_pt = from_pt_dict.get(model_name, False)
     model_url = model_download_url.get(model_name)
     config_url = model_config_url.get(model_name)
-    
+
     logger.info("downloading bert")
     model_file_name = "pytorch_model.bin" if from_pt else "tf_model.h5"
     download_file(model_url, os.path.join(model_name, model_file_name))
