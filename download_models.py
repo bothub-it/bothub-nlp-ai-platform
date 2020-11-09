6e197d92ebe8a8e48b1d91d4052b496f41bc7aec
Lucas Agra <lucasomena@hotmail.com>
2020-11-09 17:43:57 -0300

    debugging download model path

--- download_models.py
+++ download_models.py
@@ -126,7 +126,7 @@ def download_file(url, file_name):
 
 def download_bert(model_name):
     os.makedirs(model_name, exist_ok=True)
-    print("Downloading model to: ", os.path.join(os.getcwd(), model_name))
+    logger.info("Downloading model to: ", os.path.join(os.getcwd(), model_name))
 
     from_pt = from_pt_dict.get(model_name, False)
     model_url = model_download_url.get(model_name)
